9d1f7f2bbc61b6f0ce612f3d92129498011f84b7
Yu Cao <ycao@us.ibm.com>
2020-02-10 20:19:07 -0500

    debug

--- src/v1/config/config.js
+++ src/v1/config/config.js
@@ -1,13 +1,16 @@
+const fs = require('fs');
+
 const config = {};
 
 config.ocp = {};
 
-// for prod
-// config.ocp.apiserver_url = 'https://kubernetes.default';
-// config.ocp.serviceaccount_tokenpath = '/var/run/secrets/kubernetes.io/serviceaccount/token';
-
-// for test
-config.ocp.apiserver_url = 'https://api.straits.os.fyre.ibm.com:6443';
-config.ocp.serviceaccount_token = '3DdNt9wE_NLkVwqU1-bxtp3_e-8FXd5mnj_qZ0mGTnA';
+if (process.env.NODE_ENV === 'development') {
+  config.ocp.apiserver_url = process.env.API_SERVER_URL;
+  config.ocp.serviceaccount_token = process.env.SERVICEACCT_TOKEN;
+} else {
+  // SERVICE ACCOUNT TOKEN needed to review user token
+  config.ocp.apiserver_url = 'https://kubernetes.default.svc';
+  config.ocp.serviceaccount_token = fs.readFileSync('/var/run/secrets/kubernetes.io/serviceaccount/token').toString();
+}
 
 module.exports = config;

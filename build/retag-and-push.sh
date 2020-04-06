f4cfd56a79323732970e18be48db32d5b15f2577
Xuning Kang <Xuning.Kang@ibm.com>
2020-04-06 23:13:11 +0800

    fix

---
+++ build/retag-and-push.sh
@@ -0,0 +1,11 @@
+# Licensed Materials - Property of IBM
+# (c) Copyright IBM Corporation 2018, 2019. All Rights Reserved.
+# Note to U.S. Government Users Restricted Rights:
+# Use, duplication or disclosure restricted by GSA ADP Schedule
+# Contract with IBM Corp.
+
+#!/bin/bash
+COMPONENT_VERSION ?= $(shell cat ${BUILD_HARNESS_PATH}/../COMPONENT_VERSION 2> /dev/null)
+COMPONENT_TAG_EXTENSION ?= -${TRAVIS_COMMIT}
+docker tag quay.io/open-cluster-management/grc-ui-api:${COMPONENT_VERSION}${COMPONENT_TAG_EXTENSION} quay.io/open-cluster-management/grc-ui-api:dev
+docker push quay.io/open-cluster-management/grc-ui-api:dev

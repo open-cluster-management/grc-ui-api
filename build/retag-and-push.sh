748e51260f5a799e6998b3f981abb82a36c9f53b
Xuning Kang <Xuning.Kang@ibm.com>
2020-04-07 13:31:05 +0800

    Update retag-and-push.sh

--- build/retag-and-push.sh
+++ build/retag-and-push.sh
@@ -5,7 +5,7 @@
 # Contract with IBM Corp.
 
 #!/bin/bash
-export COMPONENT_VERSION=$(shell cat ${BUILD_HARNESS_PATH}/../COMPONENT_VERSION 2> /dev/null)
+# export COMPONENT_VERSION=$(shell cat ${BUILD_HARNESS_PATH}/../COMPONENT_VERSION 2> /dev/null)
 export COMPONENT_TAG_EXTENSION="-${TRAVIS_COMMIT}"
 docker tag quay.io/open-cluster-management/grc-ui-api:${COMPONENT_VERSION}${COMPONENT_TAG_EXTENSION} quay.io/open-cluster-management/grc-ui-api:dev
 docker push quay.io/open-cluster-management/grc-ui-api:dev

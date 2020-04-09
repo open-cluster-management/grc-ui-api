9652f07cba394309f8a107163e03a26da34c2802
Xuning Kang <Xuning.Kang@ibm.com>
2020-04-09 18:04:23 +0800

    Adopt build-harness

--- build/retag-and-push.sh
+++
@@ -1,9 +0,0 @@
-# Licensed Materials - Property of IBM
-# (c) Copyright IBM Corporation 2018, 2019. All Rights Reserved.
-# Note to U.S. Government Users Restricted Rights:
-# Use, duplication or disclosure restricted by GSA ADP Schedule
-# Contract with IBM Corp.
-
-#!/bin/bash
-docker tag quay.io/open-cluster-management/grc-ui-api:${COMPONENT_VERSION}${COMPONENT_TAG_EXTENSION} quay.io/open-cluster-management/grc-ui-api:dev
-docker push quay.io/open-cluster-management/grc-ui-api:dev

90099bd67bde2e844384b28919bc3cb562854594
Yu Cao <ycao@redhat.com>
2020-04-23 14:06:40 -0400

    Vendorize build harness and update copyright check

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,11 @@
+/** *****************************************************************************
+ * Licensed Materials - Property of IBM
+ * (c) Copyright IBM Corporation 2018. All Rights Reserved.
+ *
+ * Note to U.S. Government Users Restricted Rights:
+ * Use, duplication or disclosure restricted by GSA ADP Schedule
+ * Contract with IBM Corp.
+ ****************************************************************************** */
 module.exports = {
   "plugins": ["jest"],
   "extends": ["plugin:jest/recommended", "airbnb-base"],

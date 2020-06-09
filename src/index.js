efc930a440cb0076c1472ab8818fb488ceba1d48
Yu Cao <ycao@redhat.com>
2020-06-09 14:55:34 -0400

    copyright

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@
  * Use, duplication or disclosure restricted by GSA ADP Schedule
  * Contract with IBM Corp.
  ****************************************************************************** */
+/* Copyright (c) 2020 Red Hat, Inc. */
 
 import https from 'https';
 import fs from 'fs';

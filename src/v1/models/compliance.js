14006a1a8504914500f16a259abe6e48fc97d983
ChunxiAlexLuo <55596988+ChunxiAlexLuo@users.noreply.github.com>
2020-06-15 19:10:45 -0400

    save

--- src/v1/models/compliance.js
+++ src/v1/models/compliance.js
@@ -202,55 +202,6 @@ export default class ComplianceModel {
     return _.flatten(policies);
   }
 
-  // // if nsType === 'allNonClusteNS', get the list of all non-clusters namespaces
-  // // if nsType === 'allClusterNS', get the list of all clusters namespaces
-  // async getTypedNS(nsType) {
-  //   const clusterNSTemp = {};
-  //   const clusterConsoleURLTemp = {};
-
-  //   // all possible namespaces
-  //   const allNameSpace = this.kubeConnector.namespaces;
-  //   // remove cluster namespaces
-  //   const nsPromises = allNameSpace.map(async (ns) => {
-  //     const checkClustersInfoURL = `/apis/internal.open-cluster-management.io/v1beta1/namespaces/${ns}/managedclusterinfos`;
-  //     const [clustersInfo] = await Promise.all([this.kubeConnector.get(checkClustersInfoURL)]);
-  //     const clusterItems = _.get(clustersInfo, 'items');
-  //     if (Array.isArray(clusterItems) && clusterItems.length > 0) {
-  //       logger.info(`ns is ${ns} : ${JSON.stringify(clusterItems)}`);
-  //       clusterItems.forEach((item) => {
-  //         if (item.metadata && item.metadata.name
-  //             && !Object.prototype.hasOwnProperty.call(clusterNSTemp, item.metadata.name)
-  //             && item.metadata.namespace) {
-  //           // current each cluster only have one namespace
-  //           clusterNSTemp[item.metadata.name] = item.metadata.namespace;
-  //           if (item.status && item.status.consoleURL) {
-  //             clusterConsoleURLTemp[item.metadata.name] = item.spec.consoleURL;
-  //           }
-  //         }
-  //       });// if nsType === 'allClusterNS', put cluster namespaces into final result
-  //       return (nsType === 'allNonClusterNS') ? null : ns;
-  //     }// if nsType === 'allNonClusteNS', put non cluster namespaces into final result
-  //     return (nsType === 'allNonClusterNS') ? ns : null;
-  //   });
-
-  //   let nsResults = await Promise.all(nsPromises);
-  //   nsResults = nsResults.filter(ns => ns !== null);
-
-  //   const finalResult = (nsType === 'allNonClusterNS') ?
-  //     {
-  //       clusterNSTemp,
-  //       clusterConsoleURLTemp,
-  //       allNonClusterNS: nsResults,
-  //     } :
-  //     {
-  //       clusterNSTemp,
-  //       clusterConsoleURLTemp,
-  //       allClusterNS: nsResults,
-  //     };
-
-  //   return finalResult;
-  // }
-
   async getCompliances(name, namespace) {
     const urlNameSpace = namespace || (config.get('complianceNamespace') ? config.get('complianceNamespace') : 'acm');
     let policies = [];

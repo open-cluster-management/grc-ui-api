07b84d9123562a886f2e6061e60f0071baafd831
Yu Cao <ycao@redhat.com>
2020-08-24 20:31:13 -0400

    debug

--- src/v1/lib/request.js
+++ src/v1/lib/request.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 /** *****************************************************************************
  * Licensed Materials - Property of IBM
  * (c) Copyright IBM Corporation 2018. All Rights Reserved.
@@ -8,11 +9,24 @@
  ****************************************************************************** */
 /* Copyright (c) 2020 Red Hat, Inc. */
 
-const request = require('requestretry').defaults({
+const requestretry = require('requestretry');
+
+const myRetryStrategy = (err, response) => {
+  if (process.env.NODE_ENV === 'development' || process.env.DISABLE_CANARY_TEST) {
+    console.log(response);
+    console.log(err);
+    // console.log(body);
+  }
+
+  return !!err || response.statusCode === 502;
+};
+
+const request = requestretry.defaults({
   json: true,
   maxAttempts: 5,
   retryDelay: 500,
   strictSSL: false,
+  retryStrategy: myRetryStrategy,
 });
 
 export default request;

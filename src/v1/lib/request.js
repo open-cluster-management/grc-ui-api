2820af6338e0f7c0b135d64e1d8a827781480392
Yu Cao <ycao@redhat.com>
2020-08-24 21:29:54 -0400

    use logger

--- src/v1/lib/request.js
+++ src/v1/lib/request.js
@@ -10,13 +10,14 @@
 /* Copyright (c) 2020 Red Hat, Inc. */
 
 const requestretry = require('requestretry');
+const log4js = require('log4js');
+
+const logger = log4js.getLogger('server');
 
 const myRetryStrategy = (err, response) => {
-  if (process.env.NODE_ENV === 'development' || process.env.DISABLE_CANARY_TEST) {
-    if (err) {
-      console.log(err);
-      console.log(response);
-    }
+  if ((process.env.NODE_ENV === 'development' || process.env.DISABLE_CANARY_TEST) && err) {
+    logger.error(err);
+    logger.error(response);
   }
 
   return !!err || requestretry.RetryStrategies.HTTPError(err, response) || requestretry.RetryStrategies.NetworkError(err, response);

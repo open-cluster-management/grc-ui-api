116a653c13db9a7795f106cf3c9193a806f99d20
Yu Cao <ycao@redhat.com>
2020-09-30 08:49:49 -0400

    more fix

--- src/v1/lib/logger.js
+++ src/v1/lib/logger.js
@@ -13,10 +13,6 @@ const logger = log4js.getLogger('server');
 
 const log4jsConfig = process.env.LOG4JS_CONFIG ? JSON.parse(process.env.LOG4JS_CONFIG) : undefined;
 
-if (process.env.NODE_ENV !== 'production') {
-  log4js.configure(log4jsConfig || 'config/log4jsDev.json');
-} else {
-  log4js.configure(log4jsConfig || 'config/log4js.json');
-}
+log4js.configure(log4jsConfig || 'config/log4js.json');
 
 export default logger;

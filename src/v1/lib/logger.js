8d547c88f7f4a6473bd8a236aa7ebaea7390ec2f
Yu Cao <ycao@redhat.com>
2020-09-30 09:20:38 -0400

    copyright

--- src/v1/lib/logger.js
+++ src/v1/lib/logger.js
@@ -6,6 +6,7 @@
  * Use, duplication or disclosure restricted by GSA ADP Schedule
  * Contract with IBM Corp.
  ****************************************************************************** */
+/* Copyright (c) 2020 Red Hat, Inc. */
 
 import log4js from 'log4js';
 
